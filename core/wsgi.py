11fbb3e3165497b8b8b52c2e5624dd356d12ec2d
rudyc78 <103072081+rudyc78@users.noreply.github.com>
2023-03-28 13:38:26 -0600

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,16 +8,10 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-import sys
+
 
 from django.core.wsgi import get_wsgi_application
 
-###addiotanl info 
-
-path = './Render-Test/core/'
-path = '/home/usitingshit/deploy_course_work/'
-if path not in sys.path:
-  sys.path.append(path)
 
 
 ##os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
