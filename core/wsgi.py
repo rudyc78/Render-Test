8144b60482fad7b0119b5bba52e1b1f9f3972e65
rudyc78 <103072081+rudyc78@users.noreply.github.com>
2023-03-28 13:50:25 -0600

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,11 +8,13 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 
 from django.core.wsgi import get_wsgi_application
 
-
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../core')
 
 ##os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
